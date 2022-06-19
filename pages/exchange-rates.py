b491a149b656f31c91dad181c2db50707c909f47
ongkc <ahchienong@gmail.com>
2022-06-20 00:49:19 +0800

    feat: updates on exchange rates graph

--- pages/exchange-rates.py
+++ pages/exchange-rates.py
@@ -6,7 +6,7 @@ import pandas as pd
 import requests
 import json
 import datetime
-from utils.common import construct_url, call_thebank_api, prepare_df, generate_data_table, generate_filters, generate_year_dropdowns, generate_month_dropdowns
+from utils.common import construct_url, call_thebank_api, prepare_df, generate_data_table, generate_exchange_rates_filters
 
 dash.register_page(
     __name__, 
@@ -16,137 +16,89 @@ dash.register_page(
 )
 
 # CONSTANTS
-REF_RATE = "https://api.bnm.gov.my/public/kl-usd-reference-rate/year/{0}/month/{1}"
 EXCHANGE_RATE = "https://api.bnm.gov.my/public/exchange-rate"
-KIJANG_EMAS = "https://api.bnm.gov.my/public/kijang-emas"
-
-current_time = datetime.datetime.now()
 
 def generate_graph(id,title,data_frame):
+    fig = px.bar(
+            data_frame, 
+            x="currency_code", 
+            y="rate.middle_rate",
+            hover_data=['rate.buying_rate', 'rate.selling_rate'],
+            color="currency_code",
+            barmode='group',
+            title=title)
+    
+    # fig2 = make_subplots(rows=2, cols=1, row_heights=[0.8, 0.2], vertical_spacing=0, shared_xaxes=True)
+    # trace = px.Scatter(x=data_frame._data["country_code"], 
+    #             y=data_frame._data["rate.buying_rate"],
+    #             marker=dict(color=colors[len(data)]),
+    #             name=group)
+    # fig.add_trace(trace)
+    # fig.add_trace(px.line(data_frame, x="currency_code", y="rate.buying_rate"))
+    # fig.add_trace(px.bar(x="currency_code", y="rate.selling_rate"))
     return dcc.Graph(
         id=id,
-        figure=px.line(data_frame, x="date", y="rate", render_mode="svg",title=title)
+        figure=fig
     )
 
-# def generate_table(dataframe, max_rows=10):
-#     return html.Table([
-#         html.Thead(
-#             html.Tr([html.Th(col) for col in dataframe.columns])
-#         ),
-#         html.Tbody([
-#             html.Tr([
-#                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-#             ]) for i in range(min(len(dataframe), max_rows))
-#         ])
-#     ],style={'textAlign': 'left', 'border-style': 'solid'})
-
-def generate_ref_rates(options={}):
-    if options=={}:
-        options = {"year":current_time.year,"month":current_time.month}
+def generate_exchange_rates():
     
-    api_url = REF_RATE.format(list(options.values())[0],list(options.values())[1])
+    api_url = EXCHANGE_RATE
     print(api_url)
-    _ref_rates = call_thebank_api(api_url)
-    print(_ref_rates)
-    if _ref_rates['data'] != []:
-        _ref_rates_df = prepare_df(_ref_rates)
+    _exchange_rates = call_thebank_api(api_url)
+    print(_exchange_rates)
+    if _exchange_rates['data'] != []:
+        _exchange_rates_df = prepare_df(_exchange_rates)
 
         return html.Div(
-        id='ref_rates',
-        children=[html.H3(children='Reference Rates'),
+        id='exchange_rates',
+        children=[html.H3(children='Tabular Data'),
+            generate_data_table("exchange_rates-table",_exchange_rates_df),
+            generate_graph("exchange_rates-graph", "Exchange Rates (latest)",_exchange_rates_df),
+            html.Hr(),
             html.H6(children="Called API: " + api_url),
-            # html.Code(children=str(_ref_rates)),
             html.Details([
                 html.Summary('API Response'),
                 dcc.Markdown(
-                    children='```\n'+json.dumps(_ref_rates, indent=2)+'\n```'
+                    children='```\n'+json.dumps(_exchange_rates, indent=2)+'\n```'
                 )
             ]),
-            html.Br(),
-            generate_data_table("ref_rates-table",_ref_rates_df),
-            generate_graph("ref_rates-graph", "Reference Rates (" + f'{options["year"]}' + " - " + f'{options["month"]}' + ")", _ref_rates_df),
-            html.Hr()
         ])
     else:
         return html.Div(
-        id='ref_rates',
-        children=[html.H3(children='Reference Rates'),
+        id='exchange_rates',
+        children=[html.H3(children='Tabular Data'),
             html.H6(children="Called API: " + api_url),
-            # html.Code(children=str(_ref_rates)),
             html.Details([
                 html.Summary('API Response'),
                 dcc.Markdown(
-                    children='```\n'+json.dumps(_ref_rates, indent=2)+'\n```'
+                    children='```\n'+json.dumps(_exchange_rates, indent=2)+'\n```'
                 )
             ]),
             html.Br(),
             html.Div(children='Data not available'),
             html.Hr()
         ])
-    
-def generate_kijang_emas():
-    _kijang_emas = call_thebank_api(KIJANG_EMAS)
-    _kijang_emas_df = prepare_df(_kijang_emas)
-
-    return html.Div(
-    id='kijang_emas',
-    children=[html.H3(children='Kijang Emas'),
-        html.H6(children="Called API: " + KIJANG_EMAS),
-        html.Details([
-            html.Summary('API Response'),
-            dcc.Markdown(
-                children='```\n'+json.dumps(_kijang_emas_df, indent=2)+'\n```'
-            )
-        ]),
-        html.Br(),
-        # generate_dt(_kijang_emas_df),
-        # generate_graph(_kijang_emas_df),
-        html.Hr()
-    ])
-def generate_exchange_rates():
-    _exchange_rates = call_thebank_api(EXCHANGE_RATE)
-    _exchange_rates_df = prepare_df(_exchange_rates)
-
-    return html.Div(
-    id='exchange_rate',
-    children=[html.H3(children='Exchange Rates'),
-        html.H6(children="Called API: " + EXCHANGE_RATE),
-        html.Details([
-            html.Summary('API Response'),
-            dcc.Markdown(
-                children='```\n'+json.dumps(_exchange_rates, indent=2)+'\n```'
-            )
-        ]),
-        html.Br(),
-        # generate_dt(_exchange_rates_df),
-        # generate_graph(_exchange_rates_df),
-        html.Hr()
-    ])
-
-_kijang_emas = call_thebank_api(KIJANG_EMAS)
-_exchange_rates = call_thebank_api(EXCHANGE_RATE)
 
 @callback(
     Output("exchange_rates", component_property='children'),
-    Input("year-dropdown", component_property='value'),
-    Input("month-dropdown", component_property='value'),
+    Input("country-dropdown", component_property='search_value'),
+    Input("country-dropdown", component_property='value'),
     prevent_initial_call=True
 )
-def trigger_api(year_value,month_value):
-    options = {"year":year_value,"month":month_value}
-    return generate_ref_rates(options)
+def update_df(search_value,value):
+    return [
+        o for o in options if search_value in o["label"] or o["value"] in (value or [])
+    ]
+
+    
 
 layout = html.Div(children=[
+    html.Br(),
     html.H1(children='Exchange Rates'),
+    html.H6(children='Currency exchange rates from the Interbank Foreign Exchange Market in Kuala Lumpur. The price of selected countries currency in relation to Ringgit.'),
     html.Br(),
-    generate_filters(),
-    generate_ref_rates(),
+    generate_exchange_rates_filters(),
     html.Br(),
-    html.H3('--WIP--'),
-    # generate_exchange_rate()
-    html.H3(children='Exchange Rates: ('+EXCHANGE_RATE+')'),
-    html.Code(children=str(_exchange_rates)),
-    html.H3(children='Kijang Emas: ('+KIJANG_EMAS+')'),
-    html.Code(children=str(_kijang_emas)),
-
+    generate_exchange_rates()
 ])
